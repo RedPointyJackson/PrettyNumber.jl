a148219a595ac957b64f1ba2dab041bb05ea1642
redpointyjackson <a.clavero.alvarez@protonmail.com>
2017-06-23 18:47:57 +0200

    Added the source code

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # PrettyNumber
 
-[![Build Status](https://travis-ci.org/redpointyjackson/PrettyNumber.jl.svg?branch=master)](https://travis-ci.org/redpointyjackson/PrettyNumber.jl)
+[![Build Status](https://travis-ci.org/RedPointyJackson/PrettyNumber.jl.svg?branch=master)](https://travis-ci.org/RedPointyJackson/PrettyNumber.jl)
 
-[![Coverage Status](https://coveralls.io/repos/redpointyjackson/PrettyNumber.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/redpointyjackson/PrettyNumber.jl?branch=master)
+Let Julia decide how to print your numbers. No
+more boilerplate or ugly dumps!
 
-[![codecov.io](http://codecov.io/github/redpointyjackson/PrettyNumber.jl/coverage.svg?branch=master)](http://codecov.io/github/redpointyjackson/PrettyNumber.jl?branch=master)
+Before:
+
+```julia
+julia > val1 = 8595.0
+julia > val2 = 1.0e-5
+julia > println("ρ = $val1 ± $val2")
+ρ = 8595.0 ± 1.0e-5
+```
+
+Awful! Now:
+
+```julia
+julia > using PrettyNumber
+julia > val1 = 8595.0
+julia > val2 = 1.0e-5
+julia > prettyprint("ρ = $val1 ± $val2")
+ρ = 8595 ± 1⋅10⁻⁵
+```
+
+Gorgeous! Check the `test` directory to see more
+examples.
+
+Provided functions:
+* `prettify(str)`
+  Return a nicer version of the string `str`.
+* `prettyprint(str)`
+  Just `println(prettify(str))`.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,74 @@
 using PrettyNumber
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+# Check test validity
+import PrettyNumber.max_int_len
+
+@assert (max_int_len) == (5) "
+This test is prepared for other default constants.
+"
+
+import PrettyNumber.pretty
+
+@testset "Integers" begin
+    @test pretty(1)      == "1"
+    @test pretty(-11111) == "-11111"
+    @test pretty(-1e4)   == "-10000"
+    @test pretty(+1e6)   == "1⋅10⁶"
+    @test pretty(-1e6)   == "-1⋅10⁶"
+    @test pretty(-5e+60) == "-5⋅10⁶⁰"
+end
+
+@testset "Floating point" begin
+    @test pretty(3e-4)                == "3⋅10⁻⁴"
+    @test pretty(1.289e2)             == "128.9"
+    @test pretty(1.22e1)              == "12.2"
+    @test pretty(0.22e1)              == "2.2"
+    @test pretty(1.12345678)          == "1.123"
+    @test pretty(198.12345678)        == "198.123"
+    @test pretty(1988.12345678)       == "1.988⋅10³"
+    @test pretty(0.00000198812345678) == "1.988⋅10⁻⁶"
+end
+
+@testset "General macro" begin
+    @test "1"                       |> prettify == "1"
+    @test "01"                      |> prettify == "1"
+    @test "010"                     |> prettify == "10"
+    @test "100000000"               |> prettify == "1⋅10⁸"
+    @test "1e20"                    |> prettify == "1⋅10²⁰"
+    @test "1e2"                     |> prettify == "100"
+    @test "+1"                      |> prettify == "1"
+    @test "+01"                     |> prettify == "1"
+    @test "+010"                    |> prettify == "10"
+    @test "+100000000"              |> prettify == "1⋅10⁸"
+    @test "+1e20"                   |> prettify == "1⋅10²⁰"
+    @test "+1e+2"                   |> prettify == "100"
+    @test "-1"                      |> prettify == "-1"
+    @test "-01"                     |> prettify == "-1"
+    @test "-010"                    |> prettify == "-10"
+    @test "-100000000"              |> prettify == "-1⋅10⁸"
+    @test "-1e20"                   |> prettify == "-1⋅10²⁰"
+    @test "-1e-2"                   |> prettify == "-0.01"
+    @test "+1.42"                   |> prettify == "1.42"
+    @test "-01.42"                  |> prettify == "-1.42"
+    @test "+010.42"                 |> prettify == "10.42"
+    @test "-100000.42"              |> prettify == "-1.000⋅10⁵"
+    @test "2.42e-20"                |> prettify == "2.42⋅10⁻²⁰"
+    @test "-0.000000242"            |> prettify == "-2.42⋅10⁻⁷"
+    @test "+0.000000242"            |> prettify == "2.42⋅10⁻⁷"
+end
+
+@testset "Unicode expresions" begin
+    @test "ρ ≃ 0.4 ± 2e-5"         |> prettify == "ρ ≃ 0.4 ± 2⋅10⁻⁵"
+    @test "ρ ≃ 00.04 ± 2e-3"       |> prettify == "ρ ≃ 0.04 ± 0.002"
+    @test "γ=4.05e-20, β=5e-3"     |> prettify == "γ=4.05⋅10⁻²⁰, β=0.005"
+    @test "∫σ dx = 0.00004"        |> prettify == "∫σ dx = 4⋅10⁻⁵"
+    @test "0.42924042 ± 0.420e-20" |> prettify == "0.429 ± 4.2⋅10⁻²¹"
+end
+
+@testset "Escaping sequences" begin
+    num = Float64(π)
+    @test prettify("$num") == "3.142"
+end
+
+print_with_color(:green,"All test passed ✓\n")
